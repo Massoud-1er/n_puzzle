e7c62ac2640d8aed894916325e9e94b57bf1fdec
massoud <monsieurmassoud@gmail.com>
2020-03-29 17:39:54 +0200

    parsing without error protection, to use python3 src/parsing.py < example.txt

---
+++ src/parsing.py
@@ -0,0 +1,28 @@
+
+import sys
+
+lines = []
+for line in sys.stdin:
+	lines.append(line.rstrip('\n'))
+
+
+matrix = []
+
+def parse():
+    size = 0
+    for l in lines:
+        line = l.split('#')
+        if line[0].isnumeric():
+            size = int(line[0])
+        elif size > 0:
+            raw = line[0].split(' ')
+            if len(raw) >= size:
+                matrix_raw = []
+                for val in raw:
+                    if val.isnumeric() and len(matrix_raw) <= size:
+                        matrix_raw.append(int(val))
+                print(matrix_raw)
+                matrix.append(matrix_raw)
+    print(matrix)
+
+parse()
