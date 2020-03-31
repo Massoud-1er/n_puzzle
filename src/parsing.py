4f9b226b4f9dd188225e8dc91d9ccd6f8ddb17f1
massoud <monsieurmassoud@gmail.com>
2020-03-31 15:51:49 +0200

    handling wrong input file + returning the matrix in a 2D array instead of 3D and then transforming it

--- src/parsing.py
+++ src/parsing.py
@@ -7,6 +7,13 @@ for line in sys.stdin:
 
 matrix = []
 
+def print_usage():
+    print('wrong parameter: python3 main.py < input_file.txt')
+    print('/t/t/t/tThe options are as follows:')
+    print('/t/t/t/t-h [ manhattan (default), djikstra, hamming, conflict ]')
+    print('/t/t/t/t-ida Iterative Deepening A Star search Algorithm instead of default A Star Algorithm')
+    exit(1)
+
 def parse():
     matrix = []
     size = 0
@@ -15,19 +22,26 @@ def parse():
     if "unsolvable" in solvable:
         print("unsolvable !")
         exit(0)
-    
+
     for l in lines:
-        line = l.split('#')
-        if line[0].isnumeric():
-            size = int(line[0])
+        line = l.split('#')[0].rstrip()
+        if line.isnumeric() and size == 0:
+            size = int(line)
         elif size > 0:
-            raw = line[0].split(' ')
-            if len(raw) >= size:
-                matrix_raw = []
-                for val in raw:
-                    if val.isnumeric() and len(matrix_raw) <= size:
-                        matrix_raw.append(int(val))
-                # print(matrix_raw)
-                matrix.append(matrix_raw)
-    # print(matrix)
+            raw = line.split(' ')
+            count = 0
+            if len(raw) != size:
+                print_usage()
+            for val in raw:
+                if val.isnumeric():
+                    matrix.append(int(val))
+                else:
+                    print_usage()
+                count += 1
+            if count != size:
+                print_usage()
+        else:
+            print_usage()
+    print(matrix)
+    exit(1)
     return matrix
