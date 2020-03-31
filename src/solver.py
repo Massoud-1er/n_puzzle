6f7074394eee6fca42efbbb4e53b7093669d08d2
massoud <monsieurmassoud@gmail.com>
2020-03-31 04:43:50 +0200

    adding adi star alogrithm + using the right algo in function of the flags

--- src/solver.py
+++ src/solver.py
@@ -56,7 +56,7 @@ class Solver:
     def get_hash(self, graph):
         return ''.join([chr(x + 65) for x in graph])
 
-    def solve(self, graph, heuristic):
+    def a_star(self, graph, heuristic):
 
         q = queue.PriorityQueue()
         h = heuristic(graph, self.goal, self.len)
@@ -93,3 +93,47 @@ class Solver:
                         if g + 1 + new_h < seen[ghash]:
                             total_node += 1
                             q.put((g + 1 + new_h, g + 1, new_h, moves + [move], X, new_graph, ghash))
+
+    def depth_search(self, graph, moves, treshold, g, x, heuristic):
+
+        h = heuristic(graph, self.goal, self.len)
+        f = h + g
+
+        if f > treshold:
+            return f
+
+        if h == 0:
+            return moves
+
+        min_step = math.inf
+        for X, move in self.get_adj(x):
+            if 0 <= X < self.size and move != self.get_opposite_move(moves[-1]):
+                new_graph = graph[:]
+                new_graph[x], new_graph[X] = new_graph[X], new_graph[x]
+
+                ret = self.depth_search(new_graph, moves[:] + [move], treshold, g + 1, X, heuristic)
+                
+                if type(ret) == list:
+                    return ret
+                elif ret < min_step:
+                    min_step = ret
+        return min_step
+
+    def ida_star(self, graph, heuristic):
+
+        treshold = heuristic(graph, self.goal, self.len)
+
+        while True:
+            ret = self.depth_search(graph, ["start"], treshold, 0, self.zero, heuristic)
+            if type(ret) == list:
+                return ret[1:], 0, 0
+            elif ret == float('inf'):
+                break
+            else:
+                treshold = ret
+    
+    def solve(self, graph, heuristic, algo):
+        if algo == 'adi_star':
+            return self.ida_star(graph, heuristic)            
+        else:
+            return self.a_star(graph, heuristic)
